72917d43843674dc3c548964ce427fc55cbf708f
luisedmundo354 <luisedmundo354@gmail.com>
2018-09-14 16:49:15 -0500

    manager fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -75,6 +75,6 @@ class ProductsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
-    params.require(:product).permit(:name, :and, :unit, :stock, :ss, :deliver_time, :order_size, :supplier_id, :category_id ,:commentary)
+    params.require(:product).permit(:name, :brand, :unit, :stock, :ss, :deliver_time, :order_size, :supplier_id, :category_id ,:commentary)
   end
 end
